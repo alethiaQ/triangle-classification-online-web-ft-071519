0bc99b8591d08bf32a36cbd79fa8f64cd1cfcccd
Alethia Quintero <alethiaq22@gmail.com>
2019-08-01 18:43:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,10 +11,8 @@ class Triangle
   def kind
 
     if ((@side_1 + @side_2 < @side_3) || (@side_2 + @side_3 < @side_1) || (@side_1 + @side_3 < @side_2) || (@side_1 <= 0) || @side_2 <= 0 || @side_3 <= 0)
-      begin
+      
         raise TriangleError
-      rescue TriangleError => error
-      end
     end
 
     if (@side_1 == @side_2 && @side_2 == @side_3)
